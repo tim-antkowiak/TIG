22e9b298e59bf857fe0d6e43239df0ff5f5f7d08
Tim Antkowiak <antkowiak.tim@gmail.com>
2020-07-12 14:37:30 +0200

    :bug: Fix for special cases and smaller adjustments
    Changed from Region to Route Resource to be able to use POST requests
    Adjust Regex to fit all cases (hopefully)
    Increased font size and put the headline to the top right
    Create folder for image if not exists

--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageDraw, ImageFont
 import requests
+import json
 import io
+import os
 import re
 import keys
 
@@ -11,7 +13,7 @@ for plz in all_plz:
     plz_response_json = requests.get("https://geocoder.api.here.com/6.2/geocode.json?searchtext=" + str(plz) + "&country=germany&additionaldata=IncludeShapeLevel,postalCode&app_id="+ keys.app_id +"&app_code="+ keys.app_code).json()
 
     # Pull out the coordinates from the response
-    coords = re.search(r'\(\(([^()]+)\)\)', str(plz_response_json["Response"]["View"][0]["Result"][0]["Location"]["Shape"])).group(1)
+    coords = re.search(r'\(([^()]+)\)', str(plz_response_json["Response"]["View"][0]["Result"][0]["Location"]["Shape"])).group(1)
 
     coords = coords.replace(', ', ',')
 
@@ -28,7 +30,8 @@ for plz in all_plz:
     ## fc: fill color
     ## lc: line color
     ## h/w: height and width
-    map_response = requests.get('https://image.maps.ls.hereapi.com/mia/1.6/region?a='+ new_coords +'&fc=00000000&lc=FFFF0000&h=850&w=1314&apiKey=' + keys.apiKey).content
+    # map_response = requests.get('https://image.maps.ls.hereapi.com/mia/1.6/region?a='+ new_coords +'&fc=00000000&lc=FFFF0000&h=850&w=1314&apiKey=' + keys.apiKey).content
+    map_response = requests.post('https://image.maps.ls.hereapi.com/mia/1.6/route?apiKey='+keys.apiKey, data = {'r':new_coords, 'lc': 'FFFF0000', 'h':850, 'w':1314}).content
 
     #Create map image from response
     map = Image.open(io.BytesIO(map_response))
@@ -36,11 +39,13 @@ for plz in all_plz:
     # Create new (background) image
     img = Image.new('RGB', (1414, 1000), color = 'white')
     # Add PLZ to image
-    fnt = ImageFont.truetype('assets/fonts/arial.ttf', 42)
+    fnt = ImageFont.truetype('assets/fonts/arial.ttf', 52)
     d = ImageDraw.Draw(img)
-    d.text((50,50), str(plz), font=fnt, fill=(255, 0, 0))
+    d.text((1200,40), str(plz), font=fnt, fill=(255, 0, 0))
     
     # # insert Map
     img.paste(map, (50, 100))
 
-    img.save('output/' + str(plz) + '.jpg')
+    if not os.path.exists('output/' + str(plz)):
+        os.mkdir('output/' + str(plz))
+    img.save('output/' + str(plz) + '/' + str(plz) + '.jpg')
