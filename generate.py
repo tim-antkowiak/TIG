c28e82412418016235758e3a3dbab39f1b39783b
Tim Antkowiak <antkowiak.tim@gmail.com>
2020-07-17 23:13:27 +0200

    :bug: Handling multiple shapes for one PLZ
    Draw Polylines for all received shapes instead of just drawing the first.

--- generate.py
+++ generate.py
@@ -1,37 +1,42 @@
 from PIL import Image, ImageDraw, ImageFont
 import requests
-import json
 import io
 import os
 import re
 import keys
 
-all_plz = [55118]
+all_plz = [12345]
 
 for plz in all_plz:
     # Get shape coordinates for PLZ
-    plz_response_json = requests.get("https://geocoder.api.here.com/6.2/geocode.json?searchtext=" + str(plz) + "&country=germany&additionaldata=IncludeShapeLevel,postalCode&app_id="+ keys.app_id +"&app_code="+ keys.app_code).json()
+    plz_response_json = requests.get("https://geocoder.api.here.com/6.2/geocode.json?postalcode=" + str(plz) + "&country=germany&additionaldata=IncludeShapeLevel,postalCode&app_id="+ keys.app_id +"&app_code="+ keys.app_code).json()
 
-    # Pull out the coordinates from the response
-    coords = re.search(r'\(([^()]+)\)', str(plz_response_json["Response"]["View"][0]["Result"][0]["Location"]["Shape"])).group(1)
+    payload = { }
+    i = 0
+    for shape in re.finditer(r'\(([^()]+)\)', str(plz_response_json["Response"]["View"][0]["Result"][0]["Location"]["Shape"])):
+        coords = shape.group(1).replace(', ', ',')
 
-    coords = coords.replace(', ', ',')
+        # The coordinate pairs are in the false order so we have to switch each pair of lang and lat...
+        new_coords = ''
+        coords_array = coords.split(',')
+        for coord in coords_array:
+            new_coords = new_coords + coord.split(' ')[1] + ',' + coord.split(' ')[0] + ','
 
-    # The coordinate pairs are in the false order so we have to switch each pair of lang and lat...
-    new_coords = ''
-    coords_array = coords.split(',')
-    for coord in coords_array:
-        new_coords = new_coords + coord.split(' ')[1] + ',' + coord.split(' ')[0] + ','
+        new_coords = new_coords.rstrip(',')
 
-    new_coords = new_coords.rstrip(',')
+        payload['r'+str(i)] = new_coords
+
+        i += 1
 
     # Get the map with already drawed shape
-    ## a: coordinate pairs
-    ## fc: fill color
+    ## r: coordinate pairs
     ## lc: line color
     ## h/w: height and width
-    # map_response = requests.get('https://image.maps.ls.hereapi.com/mia/1.6/region?a='+ new_coords +'&fc=00000000&lc=FFFF0000&h=850&w=1314&apiKey=' + keys.apiKey).content
-    map_response = requests.post('https://image.maps.ls.hereapi.com/mia/1.6/route?apiKey='+keys.apiKey, data = {'r':new_coords, 'lc': 'FFFF0000', 'h':850, 'w':1314}).content
+    payload['lc'] = 'FFFF0000'
+    payload['h'] = 850
+    payload['w'] = 1314
+
+    map_response = requests.post('https://image.maps.ls.hereapi.com/mia/1.6/route?apiKey='+keys.apiKey, data = payload).content
 
     #Create map image from response
     map = Image.open(io.BytesIO(map_response))
